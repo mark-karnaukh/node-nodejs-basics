c3fb736e5ab5b9e6452b41a3cb7e51a0d4cbd36a
mark-karnaukh <marik13021992@gmail.com>
2022-12-04 16:44:51 +0100

    Implement command line interface(src/cli) tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
     // Write your code here 
+    const consoleArgs = process.argv;
+    const propNames = consoleArgs.filter(arg => arg.startsWith('--'));
+
+    console.log(propNames.map(propName => {
+        const propValue = consoleArgs[consoleArgs.indexOf(propName) + 1]
+
+        return `${propName.replace('--', '')} is ${propValue}`
+    }).join(', '))
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
     // Write your code here 
+    console.log(Object.entries(process.env)
+        .filter(([propName, _]) => propName.startsWith('RSS_'))
+        .map((entry) => entry.join('='))
+        .join('; ')
+    )
 };
 
 parseEnv();
