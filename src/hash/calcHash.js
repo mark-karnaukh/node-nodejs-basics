b419fcb2784e288a3dbe2272fb3b1adc3c543be8
mark-karnaukh <marik13021992@gmail.com>
2022-12-04 17:55:31 +0100

    Implement hash (src/hash) task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import { createHash } from 'crypto'
+import { readFile } from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const calculateHash = async () => {
     // Write your code here 
+    const filePath = path.resolve(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+    const fileContent = await readFile(filePath, 'utf-8')
+                                .then(data => data)
+                                .catch(error => console.error(error));
+
+
+    const hash = createHash('sha256').update(fileContent).digest('hex');
+
+    console.log(hash)
 };
 
 await calculateHash();
