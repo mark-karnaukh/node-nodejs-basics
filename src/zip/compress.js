8e080bff9491a493339010daaf629fc4390bda73
mark-karnaukh <marik13021992@gmail.com>
2022-12-05 13:40:15 +0100

    Implement zlib (src/zip) tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,25 @@
+import zlib from 'zlib';
+import fs from 'fs';
+import { pipeline } from 'stream'
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const compress = async () => {
-    // Write your code here 
+    // Write your code here
+    const gZip = zlib.createGzip();
+    const readStream = fs.createReadStream(path.resolve(__dirname, 'files', 'fileToCompress.txt'));
+    const writeStream = fs.createWriteStream(path.resolve(__dirname, 'files', 'archive.gz'));
+
+    pipeline(readStream, gZip, writeStream, (err) => {
+        if (err) {
+          console.error('An error occurred:', err);
+          process.exitCode = 1;
+        }
+    });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+import zlib from 'zlib';
+import fs from 'fs';
+import { pipeline } from 'stream'
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const decompress = async () => {
     // Write your code here 
+    const gunZip = zlib.createUnzip();
+    const readStream = fs.createReadStream(path.resolve(__dirname, 'files', 'archive.gz'));
+    const writeStream = fs.createWriteStream(path.resolve(__dirname, 'files', 'fileToCompress.txt'));
+
+    pipeline(readStream, gunZip, writeStream, (err) => {
+        if (err) {
+          console.error('An error occurred:', err);
+          process.exitCode = 1;
+        }
+    });
 };
 
 await decompress();
