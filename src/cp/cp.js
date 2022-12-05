7e80230dd962ededf55b0de406eb25849de9f2c2
mark-karnaukh <marik13021992@gmail.com>
2022-12-05 18:55:05 +0100

    Implement child processes (src/cp) tasks

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,21 @@
+import { spawn } from 'child_process'
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const spawnChildProcess = async (args) => {
     // Write your code here
+    const childProcess = spawn('node', [path.resolve(__dirname, 'files', 'script.js'), ...args], { stdio: [
+        'inherit',
+        'inherit',
+        'inherit',
+        'ipc'
+        ]
+    });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['hello', 1, true, JSON.stringify({id: 1, value: 'world'})]);
